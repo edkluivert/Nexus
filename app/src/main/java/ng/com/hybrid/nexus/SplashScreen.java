a6aa3ab7ff0861873e5ecec9cbf5f025446c2d48
Kluivert <39888432+Tristankluivert@users.noreply.github.com>
2019-01-27 14:33:23 +0100

    Initial commit

---
+++ app/src/main/java/ng/com/hybrid/nexus/SplashScreen.java
@@ -0,0 +1,38 @@
+package ng.com.hybrid.nexus;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        Thread myThread = new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+
+
+
+    }
+
+
+   }
+
